7c260ca336ad8ce4c962e219af99b679ce3c225a
coder <coder@kali.gateway>
2018-09-08 11:28:07 +1000

    starting

---
+++ server.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// index.html
+func index(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Page:%-15s Method:%s", "index.html", r.Method)
+
+	t, err := template.ParseFiles("web/index.html")
+	if err != nil {
+		log.Fatal("Template Parse Error:", err)
+	} else {
+		t.Execute(w, nil)
+	}
+}
+
+type Page struct {
+	TestCount  int
+	TestOutput []string
+}
+
+var testPg Page
+
+// test.html
+func test(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Page:%-15s Method:%s", "test.html", r.Method)
+
+	if r.Method == "POST" {
+		r.ParseForm()
+		fmt.Println("Test recv:", r.Form["comms"])
+		testPg.TestOutput = r.Form["comms"]
+		testPg.TestCount++
+
+	}
+
+	// Show page
+	t, err := template.ParseFiles("web/test.html")
+	if err != nil {
+		log.Fatal("Template Parse Error:", err)
+	} else {
+		t.Execute(w, &testPg)
+	}
+
+}
+
+func main() {
+	initSvr()
+}
+
+// initSver - Start HTTP server.
+func initSvr() {
+	log.Println("Init Server.")
+
+	// Link paths to functions
+	// When user hits / run test()
+	http.HandleFunc("/test", test)
+	http.HandleFunc("/", index)
+
+	// Serve static files
+	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
+
+	err := http.ListenAndServe("192.168.1.112:8888", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
