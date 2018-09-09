9d7e7c0fc473011d8dcb742266267966f1f63bfb
Peter Scully <peter@peterscully.name>
2018-09-09 12:48:09 +1000

    Open web pages of heritage places on info window click

--- android/app/src/main/java/name/peterscully/govhack2018/walklaunceston/HeritagePlace.kt
+++ android/app/src/main/java/name/peterscully/govhack2018/walklaunceston/HeritagePlace.kt
@@ -1,3 +1,3 @@
 package name.peterscully.govhack2018.walklaunceston
 
-class HeritagePlace(val lat: Double, val long: Double, val id: Long, val desc: String)
+class HeritagePlace(val lat: Double, val long: Double, val id: Long, val desc: String, val url: String)
